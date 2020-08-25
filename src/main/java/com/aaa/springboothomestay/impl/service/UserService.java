307e71e5b8e5014948fa7c05c61a7bbc89211216
LuoWenSheng <2659370578@qq.com>
2020-08-25 19:44:29 +0800

    wensheng

--- src/main/java/com/aaa/springboothomestay/impl/service/UserService.java
+++ src/main/java/com/aaa/springboothomestay/impl/service/UserService.java
@@ -3,7 +3,36 @@ package com.aaa.springboothomestay.impl.service;
 import com.aaa.springboothomestay.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface UserService {
     User findByDenglu (String uname,String pass);
     Integer zhuce(User user);
+
+
+    /**
+     * 查询用户个人信息
+     */
+    List<User> findByIdUser(Integer uid);
+
+    /**
+     * 修改头像
+     * @param user
+     * @return
+     */
+    Integer UpdateLead(User user);
+
+    /**
+     * 修改用户个人信息
+     * @param user
+     * @return
+     */
+    Integer UpdateUser(User user);
+
+    /**
+     * 修改密码
+     * @param pass
+     * @return
+     */
+    Integer UpdatePwd(String pass);
 }
