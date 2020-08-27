1c65bb0c8bef73a63084c8964cf6f72a201a16ef
za <1352572800@qq.com>
2020-08-27 16:15:52 +0800

    郑傲-其他类型curd

---
+++ src/main/java/com/aaa/springboothomestay/dao/HouseRulesDao.java
@@ -0,0 +1,7 @@
+package com.aaa.springboothomestay.dao;
+
+import com.aaa.springboothomestay.entity.HouseRules;
+import tk.mybatis.mapper.common.Mapper;
+@org.apache.ibatis.annotations.Mapper
+public interface HouseRulesDao extends Mapper<HouseRules> {
+}
