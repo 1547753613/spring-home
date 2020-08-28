51480bc7f475fd4bd12906ee85067283fcb27ee5
cxlyc <1497333147luo>
2020-08-28 20:12:51 +0800

    chao

--- src/main/java/com/aaa/springboothomestay/dao/HouseAddressDao.java
+++ src/main/java/com/aaa/springboothomestay/dao/HouseAddressDao.java
@@ -8,8 +8,5 @@ import java.util.List;
 
 @org.apache.ibatis.annotations.Mapper
 public interface HouseAddressDao extends Mapper<HouseAddress> {
-    @Select("select * from house_address ha " +
-            " left join house h on ha.hid = h.id" +
-            " where ha.city = #{city} ")
     List<HouseAddress> findByCity(String city);
 }
