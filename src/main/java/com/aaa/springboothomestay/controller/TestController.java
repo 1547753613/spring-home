510313b5cfc5fe0c5b617babb0cd1398e2a55414
za <1352572800@qq.com>
2020-09-10 13:47:38 +0800

    郑傲-其他类型curd

--- src/main/java/com/aaa/springboothomestay/controller/TestController.java
+++ src/main/java/com/aaa/springboothomestay/controller/TestController.java
@@ -4,12 +4,19 @@ import com.aaa.springboothomestay.aliyun.AliOSS;
 import com.aaa.springboothomestay.code.Result;
 import com.aaa.springboothomestay.code.ResultCode;
 import com.aaa.springboothomestay.code.ResultUtil;
+import com.aaa.springboothomestay.dao.OrdersDetailsDao;
 import com.aaa.springboothomestay.entity.Admins;
 import com.aaa.springboothomestay.entity.User;
+import com.aaa.springboothomestay.impl.OrdersImpl;
 import com.aaa.springboothomestay.impl.service.AdminService;
 import com.aaa.springboothomestay.impl.service.UsersService;
+import com.aaa.springboothomestay.util.AlipayConfigOne;
 import com.aaa.springboothomestay.util.MultipartFileToFile;
 import com.aaa.springboothomestay.util.UUIDUtils;
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradePagePayRequest;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -20,19 +27,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 @CrossOrigin
 @Controller
-@RequestMapping("test")
+@RequestMapping("muniao/test")
 public class TestController {
 
     @Autowired
@@ -40,7 +49,10 @@ public class TestController {
 
     @Autowired
     AdminService adminService;
-
+    @Autowired
+    OrdersDetailsDao orderDetailsDao;
+    @Resource
+    OrdersImpl ordersimp;
     @RequestMapping("/getAuth")
     @ResponseBody
     public Admins getUser4(Authentication authentication){
@@ -131,5 +143,46 @@ public class TestController {
         }
         //System.out.println(session);
         return 1;
+    }
+
+
+    @RequestMapping("/zftest")
+    public void pay(HttpServletResponse rep,Double money,Integer oid,Integer od) throws AlipayApiException, IOException {
+        //获得初始化的AlipayClient
+        AlipayClient alipayClient = new DefaultAlipayClient(AlipayConfigOne.gatewayUrl, AlipayConfigOne.app_id, AlipayConfigOne.merchant_private_key, "json", AlipayConfigOne.charset, AlipayConfigOne.alipay_public_key, AlipayConfigOne.sign_type);
+        AlipayTradePagePayRequest alipayRequest = new AlipayTradePagePayRequest();
+        alipayRequest.setReturnUrl(AlipayConfigOne.return_url);
+        alipayRequest.setNotifyUrl(AlipayConfigOne.notify_url);
+
+        //商户订单号，商户网站订单系统中唯一订单号，必填
+        String out_trade_no = UUID.randomUUID().toString().replace("-","");
+        //付款金额，必填  (随机生成 0-1000)
+        //String total_amount =String.valueOf(Math.floor(Math.random() * 1000+1));
+        String check = out_trade_no.substring(0,5);
+        orderDetailsDao.updatebyoid(oid,money,new Date(),check);
+        ordersimp.updatebyoid(od,2);
+        String total_amount =String.valueOf(money);
+        //订单名称，必填
+        String subject = "途家民宿住宿订单";
+        //商品描述，可空
+        String body = "在线支付";
+
+        alipayRequest.setBizContent("{\"out_trade_no\":\""+ out_trade_no +"\","
+                + "\"total_amount\":\""+ total_amount +"\","
+                + "\"subject\":\""+ subject +"\","
+                + "\"body\":\""+ body +"\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+
+
+        //请求
+        String result = alipayClient.pageExecute(alipayRequest).getBody();
+        rep.setContentType("text/html;charset=" + AlipayConfigOne.charset);
+        rep.getWriter().write(result);//直接将完整的表单html输出到页面
+        rep.getWriter().flush();
+        rep.getWriter().close();
+    }
+    @RequestMapping("/return_url.view")
+    public String returnUrl(HttpServletRequest request, HttpServletResponse response,HttpSession session) throws AlipayApiException, UnsupportedEncodingException, UnsupportedEncodingException {
+        return "/qiantai/tujia";
     }
 }
--- src/main/java/com/aaa/springboothomestay/dao/OrdersDetailsDao.java
+++ src/main/java/com/aaa/springboothomestay/dao/OrdersDetailsDao.java
@@ -1,9 +1,14 @@
 package com.aaa.springboothomestay.dao;
 
 import com.aaa.springboothomestay.entity.OrdersDetails;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
-
+@Component
 public interface OrdersDetailsDao {
     List<OrdersDetails> findAllOrdersDetails();
+    Integer updatebyoid(@Param("oid") Integer oid, @Param("money") Double money, @Param("dat") Date dat, @Param("check")String check);
+
 }
